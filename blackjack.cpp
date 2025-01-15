876e56dd8e5a8ba21f25ef398b1ea4e34801ab3d
wilsonwei123 <wilsonwei2023@gmail.com>
2025-01-15 17:22:05 +1300

    Created basic structure for objects

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,2 +1,21 @@
 #include "blackjack.hpp"
-//to be added
+
+void Hand::get_card() {
+    int i = rand() % counter;
+    cards.push_back(cards_left[i]);
+    char dealt_card = cards[cards.size() - 1];
+    cards_left.erase(cards_left.begin() + i);
+
+    if (dealt_card == 'a') {
+        total += 11;
+        if (total > 21) {
+            total -= 10;
+        }
+    } else if (dealt_card == 'j' || dealt_card == 'q' || dealt_card == 'k' || dealt_card == 't') {
+        total += 10;
+    } else {
+        total += dealt_card - '0';
+    }
+}
+
+//to be continued
--- blackjack.hpp
+++ blackjack.hpp
@@ -1,1 +1,14 @@
-//to be added
+#include <iostream>
+#include <vector>
+
+class Hand {
+    std::vector<char> cards_left;
+    std::vector<char> cards;
+    int total;
+    int counter;
+
+    public:
+    void get_card();
+};
+
+//to be continued
